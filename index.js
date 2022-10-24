3e5ae5c13d48e41afb6e073d97e55586ae65b2c8
jhangkar <taqit3852@gmail.com>
2022-10-25 03:08:15 +0600

    fakedata added

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const port = process.env.port || 5000
 const cors = require('cors')
 
 
+
+app.use(cors())
+
+app.get('/courses', (req,res)=>{
+    
+})
+
 app.get('/',(req,res)=>{
-    res.send('hello world!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    res.send('hello world!! app is running')
 })
 
 
