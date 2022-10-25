ddb49643be1a91233d281533e6d92dea11f1ba77
jhangkar <taqit3852@gmail.com>
2022-10-25 16:04:58 +0600

    server api done

--- index.js
+++ index.js
@@ -5,16 +5,22 @@ const app = express()
 const port = process.env.port || 5000
 const cors = require('cors')
 
-
+const courses = require('./data.json')
 
 app.use(cors())
 
-app.get('/courses', (req,res)=>{
-    
-})
 
 app.get('/',(req,res)=>{
     res.send('hello world!! app is running')
+})
+
+app.get('/courses', (req,res)=>{
+    res.send(courses)
+})
+
+app.get('/courses/:id',(req,res)=>{
+    const selectedCourse = courses.find(course => req.params.id === course._id)
+    res.send(selectedCourse)
 })
 
 
